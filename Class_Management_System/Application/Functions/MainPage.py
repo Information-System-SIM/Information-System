5f74fe52e74fe0a2a311de0cd6a52e6317cd41ff
LanAxia <80081550+LanAxia@users.noreply.github.com>
2021-05-27 05:22:13 +0800

    Update 作业管理全功能上线

--- Class_Management_System/Application/Functions/MainPage.py
+++ Class_Management_System/Application/Functions/MainPage.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from Application import models
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 from Application.Functions.function import get_image_path
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-import os
-from Class_Management_System.settings import BASE_DIR
-
->>>>>>> Stashed changes
 
 def mainpage_show(request):
     student_num = request.GET.get("student_num")
@@ -23,25 +8,7 @@ def mainpage_show(request):
     student = models.student.objects.get(student_num=student_num)
     student_name = student.student_name
     self_description = student.self_description
-<<<<<<< Updated upstream
     path = get_image_path(student_num)
-=======
-    path = "Image/" + student_num + ".jpeg"
-    default_path = "Image/default.jpg"
-    course_path = "Image/course.png"
-    dir_lst = os.listdir(os.path.join(BASE_DIR,"static/Image/"))
-    if path not in dir_lst:
-        path = default_path
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     unnoticed_homework_num = len(models.notice_homework.objects.filter(student_num_id=student_num))
     unnoticed_competition_num = len(models.notice_competition.objects.filter(student_num_id=student_num))
     unnoticed_activity_num = len(models.notice_activity.objects.filter(student_num_id=student_num))
